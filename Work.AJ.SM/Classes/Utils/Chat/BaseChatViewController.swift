32573d9dcb626bd011dda766897c951a956c6c7d
guguijun <guguijun.yz@gmail.com>
2022-03-25 10:26:07 +0800

    fix

--- Work.AJ.SM/Classes/Utils/Chat/BaseChatViewController.swift
+++ Work.AJ.SM/Classes/Utils/Chat/BaseChatViewController.swift
@@ -19,7 +19,7 @@ class BaseChatViewController: UIViewController {
     // MARK: - 拨打 | 接听 标识
     var isCalled: Bool!
     // MARK: - 呼叫类型
-    var kCallType: NIMNetCallType!
+    var kCallType: NIMNetCallMediaType!
     // MARK: - 呼叫ID
     var kCallID: UInt64!
     // MARK: - 聊天室用户，一对一时，最多两个，被叫和主叫
@@ -27,7 +27,7 @@ class BaseChatViewController: UIViewController {
     private var manager: NIMNetCallManager?
 
     
-    init(callee: String, caller: String, calltype: NIMNetCallType, callID: UInt64) {
+    init(callee: String, caller: String, calltype: NIMNetCallMediaType, callID: UInt64) {
         self.kCallee = callee
         self.kCaller = caller
         self.kCallType = calltype
@@ -54,7 +54,28 @@ class BaseChatViewController: UIViewController {
     
     
     func startCall() {
+        let callees = [kCallee!]
+        let option = setupNetCallOption()
+        option.extendMessage = "音视频请求扩展信息"
+        option.apnsContent = kCallType == .audio ? "网络通话":"视频聊天"
+        option.apnsSound = "video_chat_tip_receiver.aac"
+        option.videoCaptureParam?.startWithBackCamera = kCallType == .audio
         
+        NIMAVChatSDK.shared().netCallManager.start(callees, type: kCallType, option: option) { [weak self] error, callID in
+            guard let self = self else {
+                NIMAVChatSDK.shared().netCallManager.hangup(callID)
+                return
+            }
+            if error != nil {
+                self.showErrorMessageAndDismiss("连接失败")
+            }
+            self.kCallID = callID
+            self.chatRoomUsers.removeAll()
+            //十秒之后如果还是没有收到对方响应的control字段，则自己发起一个假的control，用来激活铃声并自己先进入房间
+            DispatchQueue.main.asyncAfter(deadline: .now()+10) {
+                self.onControl(callID, from: self.kCallee, type: .feedabck)
+            }
+        }
     }
     
     func hangUp() {
@@ -64,13 +85,12 @@ class BaseChatViewController: UIViewController {
     }
     
     func response2Call(_ accept: Bool) {
-        let option = setupNetCallOption()
+        let option = accept ? setupNetCallOption() : nil
         NIMAVChatSDK.shared().netCallManager.response(kCallID, accept: accept, option: option) {[weak self] error, callID in
             guard let self = self else { return }
             if accept {
                 if (error != nil) {
                     logger.info(" =====> 接受")
-
                 }else{
                     self.showErrorMessageAndDismiss("连接失败")
                 }
@@ -78,11 +98,97 @@ class BaseChatViewController: UIViewController {
                 self.showErrorMessageAndDismiss("")
             }
         }
+    }
+
+}
+
+extension BaseChatViewController: NIMNetCallManagerDelegate {
+    func onControl(_ callID: UInt64, from user: String, type control: NIMNetCallControlType) {
+        //多端登录时，自己会收到自己发出的控制指令，这里忽略
+        if user == NIMSDK.shared().loginManager.currentAccount() { return }
+        if callID != kCallID { return }
         
-        
+        switch control {
+        case .feedabck:
+            logger.info("收到呼叫请求的反馈，通常用于被叫告诉主叫可以播放回铃音了")
+            break
+        case .busyLine:
+            logger.info("占线")
+            break
+        case .startRecord:
+            logger.info("开始录制")
+            break
+        case .stopRecord:
+            logger.info("结束录制")
+            break
+        case .openAudio:
+            logger.info("开启了音频")
+            break
+        case .closeAudio:
+            logger.info("关闭了音频")
+            break
+        case .openVideo:
+            logger.info("开启了视频")
+            break
+        case .closeVideo:
+            logger.info("关闭了视频")
+            break
+        case .toVideo:
+            logger.info("切换到视频模式")
+            break
+        case .agreeToVideo:
+            logger.info("同意切换到视频模式，用于切到视频模式需要对方同意的场景")
+            break
+        case .rejectToVideo:
+            logger.info("拒绝切换到视频模式，用于切到视频模式需要对方同意的场景")
+            break
+        case .toAudio:
+            logger.info("切换到音频模式")
+            break
+        case .noCamera:
+            logger.info("没有可用摄像头")
+            break
+        case .background:
+            logger.info("应用切换到了后台")
+            break
+        @unknown default:
+            break
+        }
     }
     
-    // MARK: - Private
+    func onResponsed(byOther callID: UInt64, accepted: Bool) {
+        showErrorMessageAndDismiss("已在其他端处理")
+    }
+    
+    func onResponse(_ callID: UInt64, from callee: String, accepted: Bool) {
+        if kCallID == callID {
+            if accepted {
+                chatRoomUsers.append(callee)
+            }else{
+                showErrorMessageAndDismiss("对方拒绝接听")
+            }
+        }
+    }
+    
+    func onCallDisconnected(_ callID: UInt64, withError error: Error?) {
+        if kCallID == callID {
+            showErrorMessageAndDismiss("已断开")
+        }
+    }
+    
+    func onCallEstablished(_ callID: UInt64) {
+        logger.info("callid ==> \(callID) , 通话建立")
+        if kCallID == callID {
+            let result = NIMAVChatSDK.shared().netCallManager.setSpeaker(true)
+            if result {
+                logger.info("麦克风开启成功")
+            }
+        }
+    }
+}
+
+// MARK: - Private func
+extension BaseChatViewController {
     private func showErrorMessageAndDismiss(_ msg: String){
         if msg.isEmpty{
             self.dismiss(animated: true, completion: nil)
@@ -112,69 +218,5 @@ class BaseChatViewController: UIViewController {
         param.startWithBackCamera = false
         option.videoCaptureParam = param
         return option
-    }
-
-}
-
-extension BaseChatViewController: NIMNetCallManagerDelegate {
-    func onControl(_ callID: UInt64, from user: String, type control: NIMNetCallControlType) {
-        //多端登录时，自己会收到自己发出的控制指令，这里忽略
-        if user == NIMSDK.shared().loginManager.currentAccount() {
-            return
-        }
-        
-        if callID != kCallID {
-            return
-        }
-        
-        switch control {
-            
-        case .feedabck:
-            break
-        case .busyLine:
-            break
-        case .startRecord:
-            break
-        case .stopRecord:
-            break
-        case .openAudio:
-            break
-        case .closeAudio:
-            break
-        case .openVideo:
-            break
-        case .closeVideo:
-            break
-        case .toVideo:
-            break
-        case .agreeToVideo:
-            break
-        case .rejectToVideo:
-            break
-        case .toAudio:
-            break
-        case .noCamera:
-            break
-        case .background:
-            break
-        @unknown default:
-            break
-        }
-    }
-    
-    func onResponsed(byOther callID: UInt64, accepted: Bool) {
-        
-    }
-    
-    func onResponse(_ callID: UInt64, from callee: String, accepted: Bool) {
-        
-    }
-    
-    func onCallDisconnected(_ callID: UInt64, withError error: Error?) {
-        
-    }
-    
-    func onCallEstablished(_ callID: UInt64) {
-        
     }
 }
