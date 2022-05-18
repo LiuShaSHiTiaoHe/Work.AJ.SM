aa6641bcc1a5aa478a06dbf8fc8489a54678a062
guguijun <guguijun.yz@gmail.com>
2022-05-18 14:06:53 +0800

    fix

--- Work.AJ.SM/Classes/Utils/Marcos/Marcos.swift
+++ Work.AJ.SM/Classes/Utils/Marcos/Marcos.swift
@@ -9,47 +9,45 @@ import Foundation
 import UIKit
 import SwiftyUserDefaults
 
-// keyWindow
+// MARK: -  keyWindow
 let KeyWindow: UIWindow = UIApplication.shared.keyWindow!
-/// 屏幕的宽
+// MARK: -  屏幕的宽
 let kScreenWidth = UIScreen.main.bounds.size.width
-/// 屏幕的高
+// MARK: -  屏幕的高
 let kScreenHeight = UIScreen.main.bounds.size.height
+// MARK: - Scale
 let kScale = UIScreen.main.scale
-
 let kWidthScale = kScreenWidth / 375.0
 let kHeightScale = kScreenHeight / 667.0
-
+// MARK: - Device
 let kIs_iphone = (UIDevice().userInterfaceIdiom == .phone)
 let kIs_iPhoneX = (kScreenWidth >= 375.0 && kScreenHeight >= 812.0 && kIs_iphone)
-/// 间距
+// MARK: -  间距
 let kMargin: CGFloat = 20.0
 let kCellMargin: CGFloat = 10.0
-/// 圆角
+// MARK: -  圆角
 let kCornerRadius: CGFloat = 10.0
-/// 线宽
+// MARK: -  线宽
 let klineWidth: CGFloat = 0.5
-/// 双倍线宽
+// MARK: -  双倍线宽
 let klineDoubleWidth: CGFloat = 1.0
-/// 状态栏高度
+// MARK: -  状态栏高度
 let kStateHeight: CGFloat = getStatusBarHeight()
-/// 标题栏高度
+// MARK: -  标题栏高度
 let kTitleHeight: CGFloat = 44.0
-/// 状态栏和标题栏高度
-let kOriginTitleAndStateHeight: CGFloat = kStateHeight + kTitleHeight
-
-let kTitleAndStateHeight: CGFloat = kOriginTitleAndStateHeight + 4
-/// 底部导航栏高度
+// MARK: -  状态栏和标题栏高度
+let kTitleAndStateHeight: CGFloat = kStateHeight + kTitleHeight
+// MARK: -  底部导航栏高度
 let kTabBarHeight: CGFloat = (kIs_iphone ? (kIs_iPhoneX ? 83 : 49) : 49)
-/// 底部按钮高度
+// MARK: -  底部按钮高度
 let kBottomTabbarHeight: CGFloat = 49.0
-/// 底部确认按钮高度
+// MARK: -  底部确认按钮高度
 let kConfirmButtonHeight: CGFloat = 46.0
-/// 背景View的高度
+// MARK: -  背景View的高度
 let kBackgroundViewHeight: CGFloat = kScreenHeight - kTitleAndStateHeight
-/// 分段选择器的高度
+// MARK: -  分段选择器的高度
 let kJXSegmentedViewHeight: CGFloat = 40.0
-/// 侧边栏的宽度
+// MARK: -  侧边栏的宽度
 let kLeftMenuWidth: CGFloat = 260
 
 
