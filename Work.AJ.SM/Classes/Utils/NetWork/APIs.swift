d89b87566dc39ba33975785e03b260071ea330c4
guguijun <guguijun.yz@gmail.com>
2022-06-01 11:35:28 +0800

    bump version to 1.1.7

--- Work.AJ.SM/Classes/Utils/NetWork/APIs.swift
+++ Work.AJ.SM/Classes/Utils/NetWork/APIs.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // MARK: - 标记是否是生产环境。服务器地址，JPush等
-let isProduction = false
+let isProduction = true
 
 let host = isProduction ? "http://120.27.237.7:9393/" : "http://47.111.8.231:9091/"
 let servicePath = "hxcloudplus/"
